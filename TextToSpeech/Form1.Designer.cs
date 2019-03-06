d8a3eda0fecf087da2510cbb7685d844bea14b0d
谢海棠 <1622323125@qq.com>
2019-03-06 13:34:24 +0800

    添加项目文件。

---
+++ TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonResume = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "转换的文字：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(127, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(566, 107);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(125, 276);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 2;
+            this.buttonStart.Text = "试听";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(594, 276);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "保存语音";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Enabled = false;
+            this.buttonPause.Location = new System.Drawing.Point(247, 276);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 4;
+            this.buttonPause.Text = "暂停";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(469, 276);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "停止";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonResume
+            // 
+            this.buttonResume.Enabled = false;
+            this.buttonResume.Location = new System.Drawing.Point(361, 276);
+            this.buttonResume.Name = "buttonResume";
+            this.buttonResume.Size = new System.Drawing.Size(75, 23);
+            this.buttonResume.TabIndex = 6;
+            this.buttonResume.Text = "继续";
+            this.buttonResume.UseVisualStyleBackColor = true;
+            this.buttonResume.Click += new System.EventHandler(this.buttonResume_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(127, 181);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(566, 20);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(31, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "发声人：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(32, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "发声速度：";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "*.wav|*.wav|*.mp3|*.mp3";
+            this.saveFileDialog1.Title = "保存文件";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(125, 225);
+            this.trackBar1.Minimum = -10;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(568, 45);
+            this.trackBar1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 329);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonResume);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "文字转语音";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonResume;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        private SpeechSynthesizer reader;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("需要输入转换的文字", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                reader = new SpeechSynthesizer();
+                reader.Rate = trackBar1.Value;
+                reader.SelectVoice(comboBox1.Text);
+                reader.SpeakAsync(textBox1.Text.Trim());
+                buttonStart.Enabled = false;
+                buttonPause.Enabled = true;
+                buttonStop.Enabled = true;
+                reader.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(reader_SpeakCompleted);
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (reader.State == SynthesizerState.Speaking)
+            {
+                reader.Pause();
+                buttonPause.Enabled = false;
+                buttonResume.Enabled = true;
+            }
+        }
+
+        private void buttonResume_Click(object sender, EventArgs e)
+        {
+            if (reader.State == SynthesizerState.Paused)
+            {
+                reader.Resume();
+                buttonPause.Enabled = true;
+                buttonResume.Enabled = false;
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                reader.Dispose();
+                buttonStart.Enabled = true;
+                buttonPause.Enabled = false;
+                buttonStop.Enabled = false;
+                buttonResume.Enabled = false;
+            }
+        }
+
+        //event handler 
+        void reader_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            buttonStart.Enabled = true;
+            buttonPause.Enabled = false;
+            buttonStop.Enabled = false;
+            buttonResume.Enabled = false;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("需要输入转换的文字", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var _in = saveFileDialog1.ShowDialog();
+                if (_in == DialogResult.OK)
+                {
+                    if (reader != null)
+                    {
+                        reader.Dispose();
+                    }
+                    buttonStart.Enabled = false;
+                    buttonSave.Enabled = false;
+                    reader = new SpeechSynthesizer();
+                    reader.Rate = trackBar1.Value;
+                    reader.SelectVoice(comboBox1.Text);
+                    reader.SetOutputToWaveFile(saveFileDialog1.FileName);
+                    reader.SpeakAsync(textBox1.Text.Trim());
+                    reader.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(reader_SaveCompleted);
+                }
+            }
+        }
+
+        void reader_SaveCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            buttonStart.Enabled = true;
+            buttonSave.Enabled = true;
+            reader.SetOutputToNull();
+            MessageBox.Show("保存完成", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            buttonStop_Click(sender, e);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (reader == null)
+                reader = new SpeechSynthesizer();
+            var _ab = reader.GetInstalledVoices();
+            comboBox1.DataSource = _ab.Where(r=>r.VoiceInfo.Culture.Name== CultureInfo.CurrentCulture.Name).Select(r => r.VoiceInfo.Name).ToList();
+        }
+    }
+}
